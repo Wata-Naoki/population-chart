ec8fe9b27c64e652e0a22f375ac40671432a1021
Wata-Naoki <nao70blog@gmail.com>
2022-11-04 02:00:19 +0900

    全体の上下の余白を修正

--- src/components/Text/SectionTitle.tsx
+++ src/components/Text/SectionTitle.tsx
@@ -8,7 +8,7 @@ export const SectionTitle: React.FC<SectionTitleProps> = ({ children, variant = 
         variant === 'primary'
           ? ' text-left text-xl w-full ml-6 mt-5 text-neutral-500'
           : variant === 'sub'
-          ? 'text-center text-xl w-full ml-6 mb-4 text-neutral-500'
+          ? 'text-center text-xl w-full ml-6 mb-4 mt-0.5 text-neutral-400'
           : ''
       }`}
     >
