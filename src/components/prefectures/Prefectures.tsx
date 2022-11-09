4c9d653b24a087974db615d8d64bd0f3fc040a3a
Wata-Naoki <nao70blog@gmail.com>
2022-11-10 00:43:57 +0900

    buildエラー解消

---
+++ src/components/prefectures/Prefectures.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { PrefProps } from '../../types/types';
+
+//47都道府県を表示するコンポーネント
+export const Prefectures: React.FC<PrefProps> = ({ handleChange, prefData, prefList }) => {
+  return (
+    <>
+      <div className='flex flex-wrap items-center justify-start w-full gap-3 py-4 pl-6 sm:p-6'>
+        {prefData?.result?.map((pref) => {
+          return (
+            <div
+              key={pref.prefCode}
+              className={`flex gap-1 hover:shadow rounded px-1 text-zinc-500 hover:bg-zinc-400 hover:text-white hover:drop-shadow  border-transparent	transition-all hover:opacity-90	 duration-300 cursor-pointer ease-out hover:ease-in ${
+                prefList.find((prefListChild) => prefListChild.prefCode === pref.prefCode)
+                  ? 'bg-zinc-400 text-white drop-shadow-2xl shadow-2xl'
+                  : ''
+              }`}
+            >
+              {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
+              <input
+                type='checkbox'
+                id={pref.prefCode}
+                value={pref.prefCode}
+                onChange={handleChange}
+                className='cursor-pointer'
+              />
+              <label htmlFor={pref.prefCode} className='cursor-pointer'>
+                {pref.prefName}
+              </label>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
---
+++ src/pages/PrefPopulationChart.tsx
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import { Layout } from '../components/ui/Layout/Layout';
+import { PopulationGraphContainer } from '../components/populationGraph/PopulationGraph';
+import { SectionTitle } from '../components/ui/Text/SectionTitle';
+import { Title } from '../components/ui/Text/Title';
+import { Loading } from '../components/loading/Loading';
+import { useHandleSelectedData } from '../hooks/useHandleSelectedData';
+import { Prefectures } from '../components/prefectures/Prefectures';
+import { SectionLayout } from '../components/ui/Layout/SectionLayout';
+
+export const PrefPopulationChart = () => {
+  //チェックボックスを選択した際のロジック処理
+  const { prefData, isLoading, prefList, handleChange } = useHandleSelectedData();
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  return (
+    <Layout>
+      <Title>都道府県別の総人口推移</Title>
+
+      <SectionLayout>
+        <SectionTitle>都道府県</SectionTitle>
+        <Prefectures handleChange={handleChange} prefData={prefData} prefList={prefList} />
+      </SectionLayout>
+
+      <SectionLayout variant='sub'>
+        <SectionTitle variant='sub'>都道府県別人口構成グラフ</SectionTitle>
+        <PopulationGraphContainer prefList={prefList} />
+      </SectionLayout>
+    </Layout>
+  );
+};
