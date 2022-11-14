0e1c5181518e4e74cbaee33958556326c1c40b5c
渡邉尚輝 <watanabenaoki@MacBook-Air-2.local>
2022-11-15 01:13:26 +0900

    react-queryで引数がvariablesな時のfetch方法を実践。同じやつを再checkしたときにfetchしに行かなくてそこの処理をなんとかしたい

--- src/hooks/useHandleSelectedData.ts
+++ src/hooks/useHandleSelectedData.ts
@@ -1,16 +1,35 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useEffect, useRef, useState } from 'react';
+import { useQuery } from 'react-query';
 import { PopulationData } from '../types/types';
-import { usePopulationDataQuery } from './usePopulationDataQuery';
+import { fetchPopulationData, usePopulationDataQuery } from './usePopulationDataQuery';
 import { useQueryPrefData } from './usePrefDataQuery';
 
 export const useHandleSelectedData = () => {
+  const [prefList, setPrefList] = useState<PopulationData[]>([]);
+  const [selectedPref, setSelectedPref] = useState<any>({
+    prefCode: '',
+    prefName: '',
+  });
+
   // const { prefData, isLoading } = usePrefDataQuery();
   const { data: prefData, status: isLoading } = useQueryPrefData();
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const { populationData, getPopulationData } = usePopulationDataQuery();
-  const [prefList, setPrefList] = useState<PopulationData[]>([]);
+  // const { populationData, getPopulationData } = usePopulationDataQuery();
+  const {
+    isLoading: isLoadingPopulationData,
+    data: populationData,
+    refetch,
+  } = useQuery<any>({
+    queryKey: ['populationData', selectedPref],
+    queryFn: () => fetchPopulationData(selectedPref),
+  });
 
-  const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     //選択した県の番号と名前を取得
     const prefValue = prefData?.result?.find((pref) => pref.prefCode == e.target.value);
     //選択した県がすでに選択されているか確認。
@@ -23,7 +42,13 @@ export const useHandleSelectedData = () => {
       );
     } else {
       //選択されていなければ選択した県を追加するために、人口構成データをfetch
-      await getPopulationData({ prefCode: prefValue?.prefCode, prefName: prefValue?.prefName });
+      //await getPopulationData({ prefCode: prefValue?.prefCode, prefName: prefValue?.prefName });
+
+      setSelectedPref((prev: any) => ({
+        ...prev,
+        prefCode: prefValue?.prefCode,
+        prefName: prefValue?.prefName,
+      }));
     }
   };
 
@@ -34,11 +59,18 @@ export const useHandleSelectedData = () => {
         return pref.prefCode !== '';
       })
     );
-    //prefListにないpopulationDataであれば追加
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    if (!prefList.find((pref) => pref.prefCode === populationData.prefCode)) {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-      setPrefList((prev) => [...prev, populationData]);
+
+    if (populationData?.prefCode) {
+      //prefListにないpopulationDataであれば追加
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      if (!prefList.find((pref) => pref.prefCode === populationData?.prefCode)) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+        setPrefList((prev) => [...prev, populationData]);
+        // setPrefList((prev) => {
+        //   alert(prev.map((pref) => pref.prefName));
+        //   return [...prev, populationData];
+        // });
+      }
     }
   }, [populationData]);
 
--- src/hooks/usePopulationDataQuery.ts
+++ src/hooks/usePopulationDataQuery.ts
@@ -47,49 +47,47 @@ export const usePopulationDataQuery = () => {
   return { populationData, getPopulationData };
 };
 
-// export const fetchPopulationData = async ({ prefCode, prefName }: SelectedPref) => {
-//   if (prefCode) {
-//     const response = await axios.get(
-//       `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?&prefCode=${prefCode}`,
-//       {
-//         headers: {
-//           'X-API-KEY': process.env.REACT_APP_API_KEY,
-//         },
-//       }
-//     );
-//     //グラフで扱えるようにデータを整形
-//     //yearを取得。グラフのx軸に使用
-//     const yearData = response.data.result.data[0].data.map((data: { year: number }) => data.year);
-//     //valueを取得。グラフのy軸に使用
-//     const valueData = response.data.result.data[0].data.map((data: { value: number }) => data.value);
-//     const populationData = {
-//       prefCode: prefCode,
-//       prefName: prefName,
-//       yearData: yearData,
-//       valueData: valueData,
-//     };
+export const fetchPopulationData = async ({ prefCode, prefName }: SelectedPref) => {
+  if (prefCode) {
+    const response = await axios.get(
+      `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?&prefCode=${prefCode}`,
+      {
+        headers: {
+          'X-API-KEY': process.env.REACT_APP_API_KEY,
+        },
+      }
+    );
+    // グラフで扱えるようにデータを整形
+    // yearを取得。グラフのx軸に使用
+    const yearData = response.data.result.data[0].data.map((data: { year: number }) => data.year);
+    // valueを取得。グラフのy軸に使用
+    const valueData = response.data.result.data[0].data.map((data: { value: number }) => data.value);
+    const populationData = {
+      prefCode: prefCode,
+      prefName: prefName,
+      yearData: yearData,
+      valueData: valueData,
+    };
 
-//     return populationData;
-//   }
+    return populationData;
+  }
+};
+
+// export const useQueryPopulationData = () => {
+//   return useQuery<any, Error>({
+//     queryKey: 'populationData',
+//     queryFn: fetchPopulationData({ prefCode, prefName }: SelectedPref),
+//   });
 // };
-
-// // export const useQueryPopulationData = () => {
-// //   return useQuery<any, Error>({
-// //     queryKey: 'populationData',
-// //     queryFn: fetchPopulationData({ prefCode, prefName }: SelectedPref),
-// //     staleTime: Infinity,
-// //     cacheTime: Infinity,
-// //     refetchOnWindowFocus: false,
-// //   });
-// // };
 
 // //fetchPopulationDataをqueryFnに。useQueryPopulationDataを作成
 // export const useQueryPopulationData = ({ prefCode, prefName }: SelectedPref) => {
 //   return useQuery({
-//     queryKey: 'populationData',
+//     queryKey: ['populationData', { prefCode, prefName }],
 //     queryFn: () => fetchPopulationData({ prefCode, prefName }),
 //     staleTime: Infinity,
 //     cacheTime: Infinity,
 //     refetchOnWindowFocus: false,
+//     keepPreviousData: true,
 //   });
 // };
